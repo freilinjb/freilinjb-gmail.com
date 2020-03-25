109b3caf317d30f54bb9588c845f59c4eab3b3a2
freilinjb <freilinjb@gmail.com>
2020-03-25 18:36:26 -0400

    arrancando proyecto Inicial

---
+++ js/app.js
@@ -0,0 +1,37 @@
+//Variables
+const presupuestoUsuario = prompt('Cual es tu presupuesto semanal?');
+let cantidadPresupuesto;
+
+//Classes
+class Presupuesto {
+    constructor(presupuesto) {
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
+
+    }
+    //Metodo para ir restando del presupuesto actual
+    presupuestoRestante(cantidad = 0) {
+        return this.restante -= cantidad;
+    }
+}
+
+//Clase interfaz maneja todo los relacionado al HTML
+class Interfaz {
+    insertarPresupuesto(cantidad) {
+        console.log(`Cantidad: ${cantidad}`);
+        
+    }
+}
+
+//EventListener
+document.addEventListener('DOMContentLoaded', function() {
+    if(presupuestoUsuario === null || presupuestoUsuario === '') {
+        window.location.reload();
+    } else {
+        //Instanciar presupuesto
+        cantidadPresupuesto = new Presupuesto(presupuestoUsuario);
+        //Instanciar la clase de Interfaz
+        const ui = new Interfaz();
+        ui.insertarPresupuesto(cantidadPresupuesto.presupuesto)
+    }
+});
